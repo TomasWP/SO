d185a2ccf4b23ee382c95c59bc050bf1406dde80
TomasWP <tomasfonseca14@ua.pt>
2023-11-28 15:03:34 +0000

    Ficheiro swpRemove() incompleto

--- Projeto/somm23-so5g3_test/src/group/swp/swp_remove.cpp
+++ Projeto/somm23-so5g3_test/src/group/swp/swp_remove.cpp
@@ -13,8 +13,41 @@ namespace group
     {
         soProbe(406, "%s(%u)\n", __func__, idx);
 
-        /* TODO POINT: Replace next instruction with your code */
-        throw Exception(ENOSYS, __func__);
+        try {
+            // Verificar se existe algum evento
+            if (swpHead == NULL) {
+                throw Exception(EINVAL, "The swapped processes queue is empty");
+            }
+
+            // Remove o processo na posição dada
+            if (idx == 0) {
+                // Caso seja o primeiro elemento
+                SwpNode* temp = swpHead;
+                swpHead = swpHead->next;
+                delete temp;
+            } else {
+                // Caso esteja em uma posição diferente
+                SwpNode* current = swpHead;
+                SwpNode* previous = NULL;
+
+                for (uint32_t i = 0; i < idx && current != NULL; ++i) {
+                    previous = current;
+                    current = current->next;
+                }
+
+                if (current == NULL) {
+                    // A posição não existe
+                    throw Exception(EINVAL, "Invalid index for swpRemove");
+                }
+
+                previous->next = current->next;
+                delete current;
+            }
+
+        } catch (const Exception& e) {
+            // Propaga exceções
+            throw e;
+        }
     }
 
 // ================================================================================== //
